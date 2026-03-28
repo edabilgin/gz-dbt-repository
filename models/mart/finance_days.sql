652e457b597ded2519975957bef9fcef1c691f68
edaabilginn@gmail.com <edaabilginn@gmail.com>
2026-03-28 14:34:52 +0000

    finance_days mart modeli oluşturuldu

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,16 @@
+with finance_days as(
+    select
+        date_date,
+        count(orders_id) as number_of_transactions,
+        round(sum(revenue),2) as total_revenue,
+        round(sum(revenue)/count(orders_id),2) as average_basket,
+        round(sum(operational_margin),2) as total_operational_margin,
+        round(sum(purchase_cost),2) as total_purchase_cost,
+        round(sum(shipping_fee),2) as total_shipping_fee,
+        round(sum(logcost),2) as total_logcost,
+        sum(quantity) as total_quantity_product_sold
+    from {{ ref('int_orders_operational') }}
+    group by date_date 
+    )
+    select * from finance_days
+    order by date_date desc
